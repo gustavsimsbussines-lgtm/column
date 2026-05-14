258f6aee861f79fe1de5e5be65053febf737879a
Gusta <gusta@example.com>
2026-05-14 19:33:31 +0200

    Update pricing, status, and loader link

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 
 const CONFIG = {
     // Cheat Status: "UNDETECTED", "UPDATING", "TESTING", "DETECTED"
-    cheatStatus: "UPDATING",
+    cheatStatus: "UNDETECTED",
     
     // Set to false to disable the 'Download Loader' button in the portal
-    loaderEnabled: false,
+    loaderEnabled: true,
     
     // Message to show if the loader is disabled (e.g., when updating)
     disabledMessage: "Overwatch just updated to build 149597. We are currently updating offsets. Please check Discord for ETA."
